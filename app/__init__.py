8f99a77e89121464456db15ab3a9a386a9bc2e2a
yixing <leiyixing@gmail.com>
2025-06-20 15:34:14 +0100

    Create a general template

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 app = Flask(__name__)
@@ -9,3 +10,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/profile_summary')
+def profile_summary():
+    try:
+        with open('app/data/profile_summary.json', 'r') as file:
+            data = json.load(file)
+        return render_template('profile_summary.html', title="Profile Summary", profile_summary=data, url=os.getenv("URL"))
+    except FileNotFoundError:
+        return "Profile summary not found."
