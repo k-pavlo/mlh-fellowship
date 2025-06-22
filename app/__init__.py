8b9c0492537094ca393fe38116dc7f3fdbe745ea
Sw1ft0 <pasha.k460@gmail.com>
2025-06-22 20:11:41 +0100

    Optimize project's code. Implement footer everywhere
    Create layout.html template to significantly reduce amount of code in other html files by templ
    ating repeated code. Implement footer on every page using layout.html. Create footer_data.json to store global data like Name and Links. Make footer_data accesible globally

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,19 @@ import json
 load_dotenv()
 app = Flask(__name__)
 
+def load_json_file(filename):
+    path = os.path.join(app.root_path, 'data', filename)
+    with open(path, 'r') as file:
+        return json.load(file)
+
+# Make footer_data accesable globally
+@app.context_processor
+def inject_footer_data():
+    try:
+        footer_data = load_json_file('footer.json')
+        return dict(footer_data=footer_data)
+    except FileNotFoundError:
+            abort(404, description="Footer data not found.")
 
 @app.route('/')
 def index():
@@ -14,8 +27,7 @@ def index():
 @app.route('/profile_summary')
 def profile_summary():
     try:
-        with open(os.path.join(app.root_path, 'data', 'profile_summary.json'), 'r') as file:
-            data = json.load(file)
+        data = load_json_file('profile_summary.json')
         return render_template('profile_summary.html', title="Profile Summary", profile_summary=data, url=os.getenv("URL"))
     except FileNotFoundError:
         abort(404, description="Profile summary data not found.")
@@ -24,8 +36,7 @@ def profile_summary():
 def map():
     try:
         API_KEY = os.getenv("API_KEY")
-        with open(os.path.join(app.root_path, 'data', 'map.json'), 'r') as file:
-            places = json.load(file)
+        places = load_json_file('map.json')
         return render_template('map.html', title="My Travel Map", places=places, API_KEY=API_KEY, url=os.getenv("URL"))
     except FileNotFoundError:
         abort(404, description="Map data not found.")
@@ -33,8 +44,7 @@ def map():
 @app.route('/hobbies')
 def hobbies():
     try:
-        with open(os.path.join(app.root_path, 'data', 'hobbies.json'), 'r') as file:
-            data = json.load(file)
+        data = load_json_file('hobbies.json')
         return render_template('hobbies.html', title="Hobbies", hobbies=data, url=os.getenv("URL"))
     except FileNotFoundError:
         abort(404, description="Hobbies data not found.")
