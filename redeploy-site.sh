d941295ff15b7cf175e11603ee7872d6fceb2e66
Pavlo Kostianov <kostianp@tcd.ie>
2025-07-05 00:46:34 +0000

    Add comments to redeploy-site.sh. Remove unnecessary commands.

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# kill all tmux sessions
 tmux kill-server
-cd mlh-fellowship/
+
+# fetch the latest changes
 git fetch && git reset origin/main --hard
-export FLASK_ENV=development
+
+# start a detached tmux session which runs the flask server
 tmux new -d 'flask run --host=0.0.0.0'
