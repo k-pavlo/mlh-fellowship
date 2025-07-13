24c0388e77b95f834bbbce2ee1cd2a26db2cde79
Pavlo Kostianov <kostianp@tcd.ie>
2025-07-13 19:41:57 +0000

    Update redeploy-site.sh to restart service instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# kill all tmux sessions
-tmux kill-server
+# go into the project folder
+cd /root/mlh-fellowship/
 
 # fetch the latest changes
 git fetch && git reset origin/main --hard
 
-# make sure all required dependencies are installed
+# enter the python virtual environment
+source python3-virtualenv/bin/activate
+
+# make sure all required dependencies are installed 
 pip install -r requirements.txt
 
-# start a detached tmux session which runs the flask server
-tmux new -d 'flask run --host=0.0.0.0'
+# restart myportfolio service
+systemctl daemon-reload
+systemctl restart myportfolio
